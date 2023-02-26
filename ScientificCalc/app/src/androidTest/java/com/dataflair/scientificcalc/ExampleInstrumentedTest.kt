e2da4a93328a528f8155f7de160fd45c1d8787e3
SARDHA S <sardha045@gmail.com>
2023-02-26 22:01:47 +0530

    finished

--- ScientificCalc/app/src/androidTest/java/com/dataflair/scientificcalc/ExampleInstrumentedTest.kt
+++ ScientificCalc/app/src/androidTest/java/com/dataflair/scientificcalc/ExampleInstrumentedTest.kt
@@ -1,26 +1,23 @@
-package com.dataflair.scientificcalc;
+package com.dataflair.scientificcalc
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.dataflair.scientificcalc", appContext.getPackageName());
+        val appContext: Context = InstrumentationRegistry.getInstrumentation().getTargetContext()
+        assertEquals("com.dataflair.scientificcalc", appContext.getPackageName())
     }
 }
